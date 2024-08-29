7339a5c1372651632941079e4b3dd93c277cd7b9
Jan Galek <admin@gcore.cz>
2024-08-29 09:06:54 +0200

    Added tabs to 4

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+local g = vim.g
+local o = vim.o
+
+g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+g.mapleader = " "
+-- o.expandtab = true
+-- o.smartindent = true
+-- o.tabstop = 4
+-- o.shiftwidth = 4
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -26,8 +33,8 @@ require("lazy").setup({
 }, lazy_config)
 
 -- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+dofile(g.base46_cache .. "defaults")
+dofile(g.base46_cache .. "statusline")
 
 require "options"
 require "nvchad.autocmds"
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,10 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
